1eab70fd215a8aff86df0c1c5110a20de5ccffb1
Maxime Jofes <45143475+Soit0@users.noreply.github.com>
2021-01-10 20:24:23 +0100

    Rework of the project

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {HomepageComponent} from './homepage/homepage.component';
+import {AboutComponent} from './about/about.component';
+import {CurriculumComponent} from './curriculum/curriculum.component';
+import {ProjectsComponent} from './projects/projects.component';
+import {NotesComponent} from './notes/notes.component';
+import {ContactComponent} from './contact/contact.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: HomepageComponent},
+  {path: 'home', component: HomepageComponent},
+  {path: 'about', component: AboutComponent},
+  {path: 'curriculum', component: CurriculumComponent},
+  {path: 'projects', component: ProjectsComponent},
+  {path: 'notes', component: NotesComponent},
+  {path: 'contact', component: ContactComponent},
+  {path: 'login', loadChildren: () => import('./user/user.module').then(m => m.UserModule)}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,36 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {SharedModule} from './shared/shared.module';
+
+import { HomepageComponent } from './homepage/homepage.component';
+import { AboutComponent } from './about/about.component';
+import { CurriculumComponent } from './curriculum/curriculum.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { NotesComponent } from './notes/notes.component';
+import { ContactComponent } from './contact/contact.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomepageComponent,
+    AboutComponent,
+    CurriculumComponent,
+    ProjectsComponent,
+    NotesComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    SharedModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
